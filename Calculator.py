9358064fe992c63c2894a3b5ef8ff609069dbf3d
Ali Farzad <52332001+MAFarzad@users.noreply.github.com>
2022-08-09 13:01:12 -0400

    Add Multiply and Dividet methods

--- Calculator.py
+++ Calculator.py
@@ -3,3 +3,7 @@ class Calculator:
         return left + right
     def Subtract(left, right):
         return left - right
+    def Multiply(left, right):
+        return left * right
+    def Divide(left, right):
+        return left / right
